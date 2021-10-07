d30a8c4b9e5127cbfe9da3b3163fe672b8356478
mustakrakishe <mustakrkish@gmail.com>
2021-10-07 15:06:23 +0300

    Registration data validator is created and applied.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
@@ -24,12 +25,19 @@ class RegisteredUserController extends Controller
     }
     
     public function isValid(Request $request){
-        return $request->validate([
+        $input = $request->all();
+        $rules = [
             'name' => ['required', 'string', 'max:255'],
             'phone' => ['required', 'string', 'size:13', 'regex:/^\+[0-9]{12}/', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        ];
+
+        $validator = Validator::make($input, $rules);
+        
+        if($validator->fails()){
+            return $validator->errors();
+        }
     }
         
     public function store(Request $request){
--- public/js/pages/register.js
+++ public/js/pages/register.js
@@ -1,33 +1,29 @@
-function handleRegistration(event){
+function handleRegistration(){
     $('.errors').remove();
 
     let form = $('#registration');
-    // let data = new FormData(form);
     let url = $(form).attr('validationUrl');
-    let data = getFormData($(form));
+    let data = getFormData(form);
 
     $.post({
         url: url,
         data: data,
     })
-    .done(() => {
-        $(form).trigger('submit');
-    })
-    .fail(response => {
-        console.log('fail');
-        let errors = response.responseJSON.errors;
-        
-        for(let fieldName in errors){
-            let fieldErrorMessages = errors[fieldName];
-            
-            let ul = $.parseHTML('<ul class="errors mt-3 list-disc list-inside text-sm text-red-600"></ul>');
-            fieldErrorMessages.forEach(fieldErrorMessage => {
-                $(ul).append('<li>' + fieldErrorMessage + '</li>');
-            });
-            
-            $('#' + fieldName).after(ul);
+    .done(response => {
+        if(!response){
+            $(form).trigger('submit');
         }
-    })
+        else{
+            $.each(response, (fieldName, fieldErrors) => {
+                let ul = $.parseHTML('<ul class="errors mt-3 list-disc list-inside text-sm text-red-600"></ul>');
+                fieldErrors.forEach(fieldError => {
+                    $(ul).append('<li>' + fieldError + '</li>');
+                });
+                
+                $('#' + fieldName).after(ul);
+            });
+        }
+    });
 }
 
 function getFormData(form){
