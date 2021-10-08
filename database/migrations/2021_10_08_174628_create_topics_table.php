222c107083914157238bfde702110cca0293e732
mustakrakishe <mustakrkish@gmail.com>
2021-10-08 20:47:35 +0300

    Created Topic model, migration and controller. Writed create_topics_table migration.

---
+++ database/migrations/2021_10_08_174628_create_topics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('content');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
