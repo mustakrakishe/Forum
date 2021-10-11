edce78be5acd23a3b5343243609d89dce5eadec5
mustakrakishe <mustakrkish@gmail.com>
2021-10-11 15:19:58 +0300

    Configured topics routing.

--- routes/topics.php
+++ routes/topics.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('topics', TopicController::class)->middleware('auth');
+Route::resource('topics', TopicController::class)->except(['create', 'edit'])
+    ->middleware('auth');
+
+Route::resource('topics', TopicController::class)->only(['index', 'show']);
+
 Route::post('/topics/validate', [TopicController::class, 'validate'])->middleware('auth')
     ->name('topics.validate');
