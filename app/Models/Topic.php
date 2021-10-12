28a73f22c55e2a28d6fc365bf285754b89021f63
mustakrakishe <mustakrkish@gmail.com>
2021-10-12 21:57:35 +0300

    Topic model sorting global scope is created.

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,6 +15,18 @@ class Topic extends Model
         'content',
         'user_id',
     ];
+    
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope('ancient', function (Builder $builder) {
+            $builder->latest();
+        });
+    }
 
     public function author(){
         return $this->belongsTo(User::class, 'user_id');
