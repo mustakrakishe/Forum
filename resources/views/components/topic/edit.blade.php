b827b5219d0b9e72e4194edcbafa919e27632238
mustakrakishe <mustakrkish@gmail.com>
2021-10-14 14:01:14 +0300

    Created topic edit form.

---
+++ resources/views/components/topic/edit.blade.php
@@ -0,0 +1,25 @@
+@props(['topic'])
+
+<x-modal id="create-topic-modal" class="modal-lg modal-dialog-scrollable">
+    <x-slot name="title">{{ __('New topic') }}</x-slot>
+
+    <form id="create-topic-form" method="post" action="{{ route('topics.store') }}" validation="{{ route('topics.validate') }}">
+        @csrf
+
+        <div class="form-group">
+            <label for="topic-header" class="col-form-label">{{ __('Header') }}</label>
+            <input type="text" id="topic-header" class="form-control" name="header" value="{{ $topic->header }}">
+        </div>
+
+        <div class="form-group">
+            <label for="topic-description" class="col-form-label">{{ __('Description') }}</label>
+            <textarea id="topic-description" class="form-control" initHeight="250" name="description">{{ $topic->description }}</textarea>
+        </div>
+
+    </form>
+
+    <x-slot name="footer" class="bg-dark">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Cancel') }}</button>
+        <button class="btn btn-primary" form="create-topic-form">{{ __('Create') }}</button>
+    </x-slot>
+</x-modal>
