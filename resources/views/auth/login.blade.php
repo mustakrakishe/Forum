9b87787e918327b5f1ca852ab7d8fb1d64a07a34
mustakrakishe <mustakrkish@gmail.com>
2021-10-14 12:19:29 +0300

    form is submitting by enter.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                             <label for="identifier" class="col-md-4 col-form-label text-md-right">{{ __('Name or E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="identifier" type="text" class="form-control" name="identifier" value="{{ old('identifier') }}" required autocomplete="identifier" autofocus>
+                                <input id="identifier" type="text" class="form-control" name="identifier" value="{{ old('identifier') }}" autocomplete="identifier" autofocus>
                             </div>
                         </div>
 
@@ -26,7 +26,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control" name="password" autocomplete="current-password">
                             </div>
                         </div>
 
@@ -41,23 +41,24 @@
                                 </div>
                             </div>
                         </div>
-                    </form>
 
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button class="btn btn-primary" id="login-submit">
-                                {{ __('Log in') }}
-                            </button>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button class="btn btn-primary">
+                                    {{ __('Log in') }}
+                                </button>
 
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
                         </div>
-                    </div>
 
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
                             </div>
                         </div>
 
@@ -27,7 +27,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+                                <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" autocomplete="phone" autofocus>
                             </div>
                         </div>
 
@@ -35,7 +35,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email">
                             </div>
                         </div>
 
@@ -43,7 +43,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control" name="password" autocomplete="new-password">
                             </div>
                         </div>
 
@@ -51,18 +51,19 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                             </div>
                         </div>
-                    </form>
 
-                    <div class="form-group row mb-0">
-                        <div class="col-md-6 offset-md-4">
-                            <button class="btn btn-primary" id="register-submit">
-                                {{ __('Register') }}
-                            </button>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    
+                    </form>
                 </div>
             </div>
         </div>
