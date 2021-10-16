4bc5403a895b1ce3eb9a9a830de990daad064af3
mustakrakishe <mustakrkish@gmail.com>
2021-10-16 23:23:24 +0300

    Comment component views are updated.

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,24 +1,21 @@
 @props(['comment', 'pl' => 0])
 
 <div {{ $attributes->merge(['style' => 'padding-left: ' . $pl . 'px;']) }}>
-    <x-container>
+    <div class="d-flex mt-4 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-        <div class="row m-0">
-
-            <div class="col-2 pl-0 mt-2 d-flex justify-content-center">
-                <div>
-                    <div class="text-center h5">{{ $comment->author->name }}</div>
-                    <div class="text-center small text-muted">{{ substr($comment->created_at, 0, 10) }}</div>
-                    <div class="text-center small text-muted">{{ substr($comment->created_at, 11, 5) }}</div>
-                </div>
-            </div>
-
-            <div class="col">
-                <x-comment.show :comment="$comment"/>
+        <div class="pl-0 mt-1 d-flex justify-content-center" style="width: 92px;">
+            <div>
+                <div class="text-center h5">{{ $comment->author->name }}</div>
+                <div class="text-center small text-muted">{{ substr($comment->created_at, 0, 10) }}</div>
+                <div class="text-center small text-muted">{{ substr($comment->created_at, 11, 5) }}</div>
             </div>
         </div>
 
-    </x-container>
+        <div class="ml-3 w-100" style="overflow: visible;">
+            <x-comment.show :comment="$comment"/>
+        </div>
+        
+    </div>
 </div>
 
 @if(isset($comment->answer_tree))
