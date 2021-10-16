a7fb229aee0e32289e1770bfe8ae86f31d7393fe
mustakrakishe <mustakrkish@gmail.com>
2021-10-17 00:21:05 +0300

    Comment component view is updated. Added dates, actions. Comment component views are localized.

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,25 +1,21 @@
 @props(['comment', 'pl' => 0])
 
 <div {{ $attributes->merge(['style' => 'padding-left: ' . $pl . 'px;']) }}>
-    <div class="d-flex mt-4 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="d-flex mt-4 pr-4 py-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-        <div class="pl-0 mt-1 d-flex justify-content-center" style="width: 92px;">
+        <div class="mt-4 d-flex justify-content-center" style="width: 150px;">
             <div>
                 <div class="text-center h5">{{ $comment->author->name }}</div>
-                <div class="text-center small text-muted">{{ substr($comment->created_at, 0, 10) }}</div>
-                <div class="text-center small text-muted">{{ substr($comment->created_at, 11, 5) }}</div>
             </div>
         </div>
 
-        <div class="ml-3 w-100" style="overflow: visible;">
-            <x-comment.show :comment="$comment"/>
-        </div>
-        
+        <x-comment.show class="w-100" :comment="$comment"/>
+
     </div>
 </div>
 
 @if(isset($comment->answer_tree))
     @foreach($comment->answer_tree as $answer)
-        <x-comment :comment="$answer" :pl="$pl + 50"/>
+        <x-comment :comment="$answer" :pl="$pl + 70"/>
     @endforeach
 @endif
