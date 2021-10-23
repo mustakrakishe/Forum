56638bdaa12c67790f3c1d3a6067cd9370f263f1
mustakrakishe <mustakrkish@gmail.com>
2021-10-23 13:25:12 +0300

    deleted topic validate route.

--- routes/topics.php
+++ routes/topics.php
@@ -4,5 +4,3 @@ use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('topics', TopicController::class);
-
-Route::post('/topics/validate', [TopicController::class, 'xhrValidate'])->name('topics.validate');
