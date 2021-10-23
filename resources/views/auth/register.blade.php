eaa2d97ac558f6ecf38d98b265c77d52328e9096
mustakrakishe <mustakrkish@gmail.com>
2021-10-23 20:36:08 +0300

    Removed unusing localization phrases from ru.json.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,7 +32,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email">
